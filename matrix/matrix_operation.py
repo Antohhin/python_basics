16e85963578bd14efc00959da3faff201d945c18
Anton <tosha_maz@mail.ru>
2023-11-12 16:10:36 +0300

    solve matrix_operation

--- matrix/matrix_operation.py
+++ matrix/matrix_operation.py
@@ -21,9 +21,10 @@ def matrix_sum():
     return result
 
 
-ic(matrix_sum())
+# ic(matrix_sum())
 
 def matrix_multiplication():
+    
     X = [[12,7,3],
         [4 ,5,6],
         [7 ,8,9]]
@@ -35,9 +36,42 @@ def matrix_multiplication():
     result = [[0,0,0],
             [0,0,0],
             [0,0,0]]
-
+    
+    # ic(len(X))
+    # ic(len(X[0]))
+    # row_num = len(X)
+    # column_num = len(X[0])
     for row_id in range(len(X)):
         for column_id in range(len(X[0])):
-            result[row_id][column_id] = X[row_id][column_id] * Y[row_id][column_id]
+            for Yrow_id in range(len(Y)):
+                result[row_id][column_id] = result[row_id][column_id] + X[row_id][Yrow_id] * Y[Yrow_id][column_id]
+                # result[row_id][column_id] += X[row_id][Yrow_id] * Y[Yrow_id][column_id]
+                # dot_product = X[row_id][column_id] * Y[column_id][row_id]
+                # dot_product2 = dot_product
             # ic(result)
     return result
+
+# ic(matrix_multiplication())
+
+def matrix_transponse():
+
+    X = [[12,7,3],
+        [4 ,5,6],
+        [7 ,8,9]]
+
+    Y = [[5,8,1],
+        [6,7,3],
+        [4,5,9]]
+
+    result = [[0,0,0],
+            [0,0,0],
+            [0,0,0]]
+    
+    # for row_id in range(len(X)):
+        # for column_id in range(len(X[0])):
+            # result[row_id][column_id] = X[column_id][row_id]
+
+    result = [[X[j][i] for j in range(len(X[0]))] for i in range(len(X))] # через list comprehension
+    return result
+
+ic(matrix_transponse())
